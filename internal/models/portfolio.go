f5809550b66045766e7c746e7b605ef3e9df3943
karrless <konstantin.geydarov@gmail.com>
2024-11-17 20:28:37 +0300

    feat: add swagger documentation # change lib for null (guregu/null/v5 -> volatiletech/null/v9)

--- internal/models/portfolio.go
+++ internal/models/portfolio.go
@@ -1,13 +1,14 @@
 package models
 
 import (
-	"github.com/guregu/null/v5"
+	"github.com/volatiletech/null/v9"
 )
 
+// Language model
 type Language struct {
 	ID   int64       `form:"id" json:"id" db:"id"`
 	Name string      `form:"name" json:"name" db:"name"`
-	Svg  null.String `form:"svg" json:"svg" db:"svg"`
+	Svg  null.String `form:"svg" json:"svg" db:"svg" swaggertype:"string"`
 }
 
 type Project struct {
@@ -17,12 +18,12 @@ type Project struct {
 	Description  string      `form:"dscription" json:"dscription" db:"description"`
 	LanguageID   int64       `form:"languageID" json:"languageID" db:"lang_id"`
 	Language     *Language   `form:"language" json:"language" db:"-"`
-	IsActive     null.Bool   `form:"isActive" json:"isActive" db:"is_active"`
-	IsArchived   null.Bool   `form:"isArchived" json:"isArchived" db:"is_archived"`
-	IsDeveloping null.Bool   `form:"isDeveloping" json:"isDeveloping" db:"is_developing"`
-	GHLink       null.String `form:"GHLink" json:"GHLink" db:"gh_link"`
-	TGLink       null.String `form:"TGLink" json:"TGLink" db:"tg_link"`
-	HTTPLink     null.String `form:"HTTPLink" json:"HTTPLink" db:"http_link"`
+	IsActive     null.Bool   `form:"isActive" json:"isActive" db:"is_active" swaggertype:"boolean"`
+	IsArchived   null.Bool   `form:"isArchived" json:"isArchived" db:"is_archived" swaggertype:"boolean"`
+	IsDeveloping null.Bool   `form:"isDeveloping" json:"isDeveloping" db:"is_developing" swaggertype:"boolean"`
+	GHLink       null.String `form:"GHLink" json:"GHLink" db:"gh_link" swaggertype:"string"`
+	TGLink       null.String `form:"TGLink" json:"TGLink" db:"tg_link" swaggertype:"string"`
+	HTTPLink     null.String `form:"HTTPLink" json:"HTTPLink" db:"http_link" swaggertype:"string"`
 }
 
 type ProjectFilter struct {
--- internal/transport/rest/server.go
+++ internal/transport/rest/server.go
@@ -2,10 +2,13 @@ package rest
 
 import (
 	"context"
+	"gowebsite/docs"
 	"gowebsite/internal/transport/rest/routes"
 	"gowebsite/pkg/db/postgres"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type RESTServer struct {
@@ -17,13 +20,20 @@ func NewRESTServer(ctx context.Context, db *postgres.DB, port string) *RESTServe
 	r := gin.Default()
 
 	r.SetTrustedProxies([]string{"127.0.0.1", "192.168.31.157"})
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Host = "localhost:" + port
+	docs.SwaggerInfo.Title = "KarrlessGo API"
+	docs.SwaggerInfo.Description = "API for Karrless.ru website"
+	docs.SwaggerInfo.Version = "1.0"
+
+	r.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
 
 	api := r.Group("/api")
-
 	v1 := api.Group("/v1")
 
 	routes.PortfolioRoutes(ctx, v1, db)
 
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return &RESTServer{r: r, port: port}
 }
 
